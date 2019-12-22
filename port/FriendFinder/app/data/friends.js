bde457273f2e8a5137aaace2c673e31f57de2b83
Rubinho Lima <54456672+rubinholima@users.noreply.github.com>
2019-12-22 15:01:52 -0500

    Add FriendsFinder to portfolio

---
+++ port/FriendFinder/app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+	
+	{
+		name: "Joanne Shiffer",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRLqgY70lBSZumIwlEUvDnl9iZzm5637Z2t6fcWM6M51dVnvQT1&s",
+		scores: [
+			"4",
+			"1",
+			"4",
+			"3",
+			"1",
+			"5",
+			"1",
+			"3",
+			"3",
+			"1"
+		]
+	},
+	{
+		name: "Sandra Jeisse",
+		photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ1jGarHg0HoRnKMcLTzHo9fPhgbKtlZQsdyFnZmaa8TpS7Xsi4&s",
+		scores: [
+			"4",
+			"1",
+			"5",
+			"3",
+			"1",
+			"4",
+			"1",
+			"1",
+			"5",
+			"3"
+		]
+	},
+	{
+		name: "Julius Brown",
+		photo: "https://www.travelvisapro.com/wp-content/uploads/2018/05/smile1.jpg",
+		score: [
+			"3",
+			"1",
+			"4",
+			"1",
+			"4",
+			"3",
+			"5",
+			"1",
+			"5",
+			"4"		
+		]
+	}
+];
+
+module.exports = friends;
